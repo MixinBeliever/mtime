e8a659782f85fbdc90ed07a7a11510b6f8122866
TengMingSun <1838699551@qq.com>
2019-03-29 09:48:16 +0800

    上午提交一次

--- src/views/Buy/Coming.vue
+++ src/views/Buy/Coming.vue
@@ -41,20 +41,37 @@
     </h1>
 
     <div class="content">
+      <p class="month">4月</p>
       <ul>
-        <router-link tag="li" to="" v-for="items in datalistMv" :key="items.id">
-          <p class="Month">{{items.rMonth}}月</p>
-          <span class="Day">{{items.rDay}}日</span>
-          <img :src="items.image" alt="">
+        <router-link tag="li" to="" v-for="items in datalistMv" :key="items.id" v-if="items.image">
+          <p class="day">{{items.rDay}}日</p>
+          <img :src="items.image" alt="" >
           <div class="titleM">
             <p>{{items.title}}</p>
             <p><span>{{items.wantedCount}}</span>人想看 - {{items.type}}</p>
             <p>导演：{{items.director}}</p>
-            <button>超前预售</button>
-            <button>预告片</button>
+            <button v-if="items.isTicket" class="tick">超前预售</button>
+            <button v-if="items.isVideo" class="video">预告片</button>
           </div>
         </router-link> 
       </ul>
+      <div>
+         <p class="month">5月</p>
+         <ul>
+        <router-link tag="li" to=""  v-for="itemss in datalistMvH" :key="itemss.id">
+          <p class="day">{{itemss.rDay}}日</p>
+          <img :src="itemss.image" alt="" >
+          <div class="titleM">
+            <p>{{itemss.title}}</p>
+            <p><span>{{itemss.wantedCount}}</span>人想看 - {{itemss.type}}</p>
+            <p>导演：{{itemss.director}}</p>
+            <button v-if="itemss.isTicket" class="tick">超前预售</button>
+            <button v-if="itemss.isVideo" class="video">预告片</button>
+          </div>
+        </router-link> 
+      </ul>
+      </div>
+     
     </div>
 
   </div>
@@ -64,12 +81,15 @@ import vue from "vue"
 import Swiper from "swiper";
 import axios from "axios";
 import "swiper/dist/css/swiper.min.css";
+import { loadavg } from 'os';
 
 export default {
   data() {
     return {
       datalist: [],
-      datalistMv:[]
+      datalistMv:[],
+      datalistMvH:[],
+      
     };
   },
   mounted() {
@@ -77,17 +97,23 @@ export default {
       url:
         "/Service/callback.mi/Movie/MovieComingNew.api?locationId=290&t=20193281635090995"
     }).then(res => {
+        console.log(res.data.moviecomings)
+      this.datalistMvH = res.data.moviecomings
+      this.datalistMvH = res.data.moviecomings.splice(-5,5)
+      //console.log(this.datalistMvH)
       this.datalist = res.data.attention;
 
-      console.log(res.data.moviecomings)
+      //console.log(res.data.moviecomings)
       this.datalistMv = res.data.moviecomings
+      this.datalistMv.length = 27
       this.$nextTick(() => {
         var mySwiper = new Swiper(".swiper-container", {
           slidesPerView: 1.1
         });
       });
     });
-  }
+
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -103,9 +129,7 @@ export default {
       color: #b3b1b1;
     }
   }
-   
 
- 
   .swiperP{
       position: relative;
       background: #fff;
@@ -176,10 +200,62 @@ export default {
   }
   // 即将预售
   .content{
+    .month{
+      line-height: 50px;
+      border-bottom: 1px solid #8e8e8e;
+      margin-left: 20px;
+    }
       ul{
         li{
+           margin-left: 20px;
+           height: 150px;
+           border-bottom: 1px solid #8e8e8e;
+           padding-top: 20px;
+           .day{
+             float: left;
+             margin-right: 10px;
+           }
             img{
-              width: 30%;
+              width: 23%;
+              float: left;
+              margin-right: 20px;
+            }
+            .titleM{
+              p:nth-of-type(1){
+                font-size: 20px;
+                font-weight: bold;
+              }
+               p:nth-of-type(2){
+                font-size: 16px;
+                margin: 5px 0;
+                span{
+                  color: #f89d52;
+                }
+              }
+              p:nth-of-type(3){
+                font-size: 16px;
+              }
+              p:nth-of-type(4){
+                font-size: 16px;
+              }
+              button{
+                width: 90px;
+                height: 40px;
+                margin-top: 10px;
+                margin-right: 10px;
+                border-radius: 35px;
+                border: none;
+                font-size: 16px;
+              }
+              button.tick{
+                background: #f89d52;
+                color: #fff;
+              }
+              button.video{
+                background: #fff;
+                color: rgb(106, 238, 117);
+                border: 1px solid rgb(106, 238, 117);
+              }
             }
         }
       }
